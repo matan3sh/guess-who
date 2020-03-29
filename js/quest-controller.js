63c86a487cded6d1414b88dad31a683eeb3a5dc4
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-03-29 16:57:48 +0300

    beautify code

--- js/quest-controller.js
+++ js/quest-controller.js
@@ -25,12 +25,12 @@ function renderQuest() {
 function onUserResponse(res) {
     if (isChildless(getCurrQuest())) {
         if (res === 'yes') {
-            $('.modal').show()
+            showWinnigModal()
         } else {
             alert('I dont know...teach me!')
             $('.quest').hide()
             $('.new-quest').show()
-            $('.alert-modal').show()
+            showAlertModal()
         }
     } else {
         gLastRes = res
@@ -54,6 +54,37 @@ function onRestartGame() {
     onCloseModal()
     gLastRes = null;
     restartGame()
+}
+
+function showWinnigModal() {
+    var strHtml = ` <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Victorius!</h5>
+                                <button type="button" class="btn btn-dark" onclick="onCloseModal()" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body text-center">
+                                <p>You Win</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" onclick="onRestartGame()">Restart game</button>
+                            </div>
+                        </div>
+                    </div>
+    `
+    $('.modal').html(strHtml)
+    $('.modal').show()
+}
+
+function showAlertModal() {
+    var strHtml = ` <h4 class="alert-heading">Help to make us better</h4>
+                    <hr>
+                    <p class="mb-0">Unfortunately, we were unable to identify what you were thinking of, but we would love to add it to our repository</p>
+    `
+    $('.alert-modal').html(strHtml)
+    $('.alert-modal').show()
 }
 
 function onCloseModal() {
